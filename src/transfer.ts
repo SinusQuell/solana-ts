6e44f5a2d815debefc1c5820981b64f42f64c80f
SinusQuell <Boddafree@gmail.com>
2023-12-27 13:53:26 +0100

    03 write data to the network

--- src/transfer.ts
+++ src/transfer.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { Connection, Transaction, Keypair, PublicKey, clusterApiUrl, sendAndConfirmTransaction, SystemProgram  } from "@solana/web3.js";
+import { Connection, Transaction, Keypair, PublicKey, clusterApiUrl, sendAndConfirmTransaction, SystemProgram, LAMPORTS_PER_SOL  } from "@solana/web3.js";
 import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
 import Validator from "./lib/validator"
 
@@ -14,15 +14,20 @@ const connection = new Connection(clusterApiUrl("devnet"))
 
 // acquire sender and receiver public keys
 let receiverPublicKey: PublicKey = await Validator.validatePublicKey(suppliedPublicKey, connection)
-let senderPublicKey: PublicKey = getKeypairFromEnvironment("SECRET_KEY").publicKey
+let senderKeyPair: Keypair = getKeypairFromEnvironment("SECRET_KEY")
 
-console.log(`✅ Sending \x1b[32m1 SOL\x1b[0m from \x1b[35m${senderPublicKey}\x1b[0m to \x1b[36m${receiverPublicKey}\x1b[0m.`); 
+const LAMPORTS_TO_SEND = 1000000000;
+console.log(`✅ Sending \x1b[32m${LAMPORTS_TO_SEND / LAMPORTS_PER_SOL} SOL\x1b[0m from \x1b[35m${senderKeyPair.publicKey}\x1b[0m to \x1b[36m${receiverPublicKey}\x1b[0m.`); 
 
-// // create transaction
-// const transaction = new Transaction().add(
-//     SystemProgram.transfer({
-//         fromPubkey: senderPublicKey,
-//         toPubkey: receiverPublicKey,
-//         lamports: 1 * 1000000000,
-//     })
-// );
+// create transaction
+const transaction = new Transaction().add(
+    SystemProgram.transfer({
+        fromPubkey: senderKeyPair.publicKey,
+        toPubkey: receiverPublicKey,
+        lamports: LAMPORTS_TO_SEND,
+    })
+);
+
+// send and confirm transaction, log transaction signature
+const signature = await sendAndConfirmTransaction(connection, transaction, [senderKeyPair]);
+console.log(`✅ Success! Transaction Signature: \x1b[32m${signature}\x1b[0m`);
